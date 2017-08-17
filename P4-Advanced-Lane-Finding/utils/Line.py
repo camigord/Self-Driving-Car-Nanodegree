2b5f752021d69bfe5c6a3e9745f0f8371f3ff17a
camigord <camigord@gmail.com>
2017-08-17 18:04:42 +0200

    good results in challenging video

--- P4-Advanced-Lane-Finding/utils/Line.py
+++ P4-Advanced-Lane-Finding/utils/Line.py
@@ -1,23 +1,51 @@
+import numpy as np
 
 class Line():
     def __init__(self):
         # was the line detected in the last iteration?
         self.detected = False
-        # x values of the last n fits of the line
-        self.recent_xfitted = []
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None
-        #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None
         #polynomial coefficients for the most recent fit
         self.current_fit = [np.array([False])]
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float')
-        #x values for detected line pixels
-        self.allx = None
-        #y values for detected line pixels
-        self.ally = None
+
+        self.k = 3
+        self.last_fits = []
+        self.radius_of_curvature = []
+        self.line_base_pos = []
+        for i in range(self.k):
+            self.last_fits.append(None)
+            self.radius_of_curvature.append(None)
+            self.line_base_pos.append(None)
+
+    def add_stats(self, detected, fit, curvature, distance):
+        self.detected = detected
+        if self.detected:
+            self.current_fit = fit
+            self.last_fits.append(fit)
+            self.radius_of_curvature.append(curvature)
+            self.line_base_pos.append(distance)
+        else:
+            self.last_fits.append(None)
+            self.radius_of_curvature.append(None)
+            self.line_base_pos.append(None)
+
+        self.last_fits = self.last_fits[1:]
+        self.radius_of_curvature = self.radius_of_curvature[1:]
+        self.line_base_pos = self.line_base_pos[1:]
+
+
+    def get_best_fit(self):
+        temp = np.zeros([3])
+        curv = 0
+        dist = 0
+        count = 0
+        for i in range(self.k):
+            if self.last_fits[i] is not None:
+                temp += self.last_fits[i]
+                curv += self.radius_of_curvature[i]
+                dist += self.line_base_pos[i]
+                count += 1
+
+        if count == 0:
+            return None
+        else:
+            return (temp/float(count), curv/float(count), dist/float(count))
--- P4-Advanced-Lane-Finding/utils/utils.py
+++ P4-Advanced-Lane-Finding/utils/utils.py
@@ -4,42 +4,37 @@ import cv2
 ym_per_pix = 30/720 # meters per pixel in y dimension
 xm_per_pix = 3.7/700 # meters per pixel in x dimension4
 
+# Define source points for perspective transformation
+# src_points = np.float32([[596,447],[685,447],[1115,720],[196,720]])
+src_points = np.float32([[200,720],[453,547],[835,547],[1100,720]])
+# Define 4 destination points
+#dst = np.float32([[offset, 0], [img_size[0]-offset, 0],[img_size[0]-offset, img_size[1]],[offset, img_size[1]]])
+dst_points = np.float32([[320,720],[320,576],[960,576],[960,720]])
+
 def undistort(img,mtx,dist):
     '''
     Undistort an image given the camera calibration coefficients
     '''
     return cv2.undistort(img, mtx, dist, None, mtx)
 
-def get_eagle_eye(img, src):
+def get_eagle_eye(img):
     '''
     Gets eagle eye perspective of the source points in the current image. The destination points
     are calculated based on offset
     '''
     img_size = (img.shape[1],img.shape[0])
 
-    # Define 4 destination points
-    #dst = np.float32([[offset, 0], [img_size[0]-offset, 0],[img_size[0]-offset, img_size[1]],[offset, img_size[1]]])
-    dst = np.float32([[320,720],[320,576],[960,576],[960,720]])
-
-    w,h = 1280,720
-    x,y = 0.5*w, 0.8*h
-    # Define 4 destination points
-    dst = np.float32([[(w-x)/2.,h],
-                  [(w-x)/2.,0.82*h],
-                  [(w+x)/2.,0.82*h],
-                  [(w+x)/2.,h]])
-
-
     # Get the transform matrix
-    M = cv2.getPerspectiveTransform(src, dst)
+    M = cv2.getPerspectiveTransform(src_points, dst_points)
     # Warp image to a top-down view
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-    return warped, dst
+    return warped
 
-def get_inverse_transform(img, src, dest):
+def get_inverse_transform(img):
     img_size = (img.shape[1],img.shape[0])
-    Minv = cv2.getPerspectiveTransform(dst, src)
+    Minv = cv2.getPerspectiveTransform(dst_points, src_points)
     warped = cv2.warpPerspective(img, Minv, img_size, flags=cv2.INTER_LINEAR)
+    return warped
 
 def draw_points(img, points):
     temp = np.copy(img)
@@ -52,49 +47,54 @@ def draw_points(img, points):
 def get_binary_image(img, s_thresh=(170, 255), sx_thresh=(20, 100), h_thresh=(20,30)):
     img = np.copy(img)
 
-    # Convert to HSV color space
+    # Convert to HSV color space and separate the V channel
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
-    l_channel = hsv[:,:,1]
     s_channel = hsv[:,:,2]
-    h_channel = hsv[:,:,0]
 
-    # Sobel x
-    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    thr = (20,100)
+    kernel_size = 15
+    channel = cv2.GaussianBlur(s_channel, (kernel_size, kernel_size), 0)
+    sobelx = cv2.Sobel(channel, cv2.CV_64F, 1, 0) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
     scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
 
-    # Threshold x gradient
-    sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    sxbinary_s = np.zeros_like(scaled_sobel)
+    sxbinary_s[(scaled_sobel >= thr[0]) & (scaled_sobel <= thr[1])] = 1
 
-    # Threshold saturation channel
-    s_binary = np.zeros_like(s_channel)
-    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Convert to YUV
+    yuv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV).astype(np.float)
+    u_channel = yuv[:,:,2]
 
-    # Threshold hue channel
-    h_binary = np.zeros_like(h_channel)
-    h_binary[(h_channel >= h_thresh[0]) & (h_channel <= h_thresh[1])] = 1
+    channel = cv2.GaussianBlur(u_channel, (kernel_size, kernel_size), 0)
+    sobelx = cv2.Sobel(channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
 
-    # color_binary = np.dstack(( h_binary, sxbinary, s_binary))
-    # Combine binaries
-    combined = np.zeros_like(scaled_sobel)
-    combined[(h_binary == 1) | (sxbinary == 1) | (s_binary == 1)] = 1
+    sxbinary_u = np.zeros_like(scaled_sobel)
+    sxbinary_u[(scaled_sobel >= thr[0]) & (scaled_sobel <= thr[1])] = 1
+
+    R = img[:,:,0]
+    thresh = (180, 255)
+    binary = np.zeros_like(R)
+    binary[(R > thresh[0]) & (R <= thresh[1])] = 1
+
+    combined = np.zeros_like(binary)
+    combined[(binary == 1) | ((sxbinary_s == 1) & (sxbinary_u == 1))] = 1   # (h_binary == 1) |
     return combined
 
 def process_frame(frame,mtx,dist):
     # Undistort image
     undistorted = undistort(frame,mtx,dist)
     # Threshold image with default values
-    thresholded = get_binary_image(undistorted)
-
-    # Define source points for perspective transformation
-    # src_points = np.float32([[596,447],[685,447],[1115,720],[196,720]])
-    src_points = np.float32([[200,720],[453,547],[835,547],[1100,720]])
+    #thresholded = get_binary_image(undistorted)
 
     # Change perspective to eagle-eye view
-    top_down, dst_points = get_eagle_eye(thresholded, src_points)
+    #top_down = get_eagle_eye(thresholded)
 
-    return top_down
+    top_down = get_eagle_eye(undistorted)
+    thresholded = get_binary_image(top_down)
+
+    return undistorted, thresholded, top_down
 
 def find_lanes_hist(top_down):
     # Take a histogram of the bottom half of the image
@@ -107,7 +107,34 @@ def find_lanes_hist(top_down):
 
     return leftx_base, rightx_base
 
-def get_polynomial(top_down, leftx_base, rightx_base, nwindows = 9, margin=100, minpix = 50, debug=True):
+def get_poly_from_last(binary_warped, left_fit, right_fit, margin=100):
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    '''
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    '''
+    left_curverad, right_curverad = get_curvature(leftx, lefty, rightx, righty)
+
+    return left_fit, right_fit, left_curverad, right_curverad
+
+
+def get_polynomial(top_down, leftx_base, rightx_base, nwindows = 8, margin=100, minpix = 50, debug=False):
     '''
     Implementing a sliding window approach to detect points in left and right lanes and fit a second order polynomial
         nwindows is the number of sliding windows
@@ -194,15 +221,25 @@ def get_curvature(leftx, lefty, rightx, righty):
 
 def sanity_check(left_fitx, right_fitx, left_curverad, right_curverad):
     # 1. Check that curvature is within the same order of magnitude.
-    if left_curverad < 300 or left_curverad > 700 or right_curverad < 300 or right_curverad > 700:
+    if left_curverad < 150 or right_curverad < 150:
         return False
 
     # 2. Check that the horizontal distance is close to the estimated 3.7 meters.
     # I assume lanes are parallel if the distance between all horizontal points is similar.
+    '''
     dist = (right_fitx - left_fitx) * xm_per_pix    # Horizontal distance between points (in meters)
     m = np.mean(dist)
     std = np.std(dist)
-    if m+std > 4.0 or m-std < 3.4:  # If distance remains within given range [3.4, 4.0]
+    if m+std > 4.5 or m-std < 2.0:  # If distance remains within given range [3.0, 4.0]
+        print(m, std)
         return False
-
+    '''
     return True
+
+def find_position(image_size, left_point, right_point):
+    # Find the position of the car from the center
+    middle_of_image = image_size/2
+
+    center_of_road = (left_point + right_point)/2
+    # Define conversions in x and y from pixels space to meters
+    return (middle_of_image - center_of_road)*xm_per_pix
