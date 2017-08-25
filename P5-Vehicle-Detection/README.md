908464e4ae73208a6025c970f9778caaf66fd3af
camigord <camigord@gmail.com>
2017-08-25 16:27:06 +0200

    implementing frame processor and detection class

--- P5-Vehicle-Detection/README.md
+++ P5-Vehicle-Detection/README.md
@@ -8,7 +8,7 @@ Creating a great writeup:
 ---
 A great writeup should include the rubric points as well as your description of how you addressed each point.  You should include a detailed description of the code used in each step (with line-number references and code snippets where necessary), and links to other supporting documents or external references.  You should include images in your writeup to demonstrate how your code works with examples.  
 
-All that said, please be concise!  We're not looking for you to write a book here, just a brief description of how you passed each rubric point, and references to the relevant code :). 
+All that said, please be concise!  We're not looking for you to write a book here, just a brief description of how you passed each rubric point, and references to the relevant code :).
 
 You can submit your writeup in markdown or use another method and submit a pdf instead.
 
@@ -18,7 +18,7 @@ The Project
 The goals / steps of this project are the following:
 
 * Perform a Histogram of Oriented Gradients (HOG) feature extraction on a labeled training set of images and train a classifier Linear SVM classifier
-* Optionally, you can also apply a color transform and append binned color features, as well as histograms of color, to your HOG feature vector. 
+* Optionally, you can also apply a color transform and append binned color features, as well as histograms of color, to your HOG feature vector.
 * Note: for those first two steps don't forget to normalize your features and randomize a selection for training and testing.
 * Implement a sliding-window technique and use your trained classifier to search for vehicles in images.
 * Run your pipeline on a video stream (start with the test_video.mp4 and later implement on full project_video.mp4) and create a heat map of recurring detections frame by frame to reject outliers and follow detected vehicles.
@@ -31,3 +31,93 @@ Some example images for testing your pipeline on single frames are located in th
 **As an optional challenge** Once you have a working pipeline for vehicle detection, add in your lane-finding algorithm from the last project to do simultaneous lane-finding and vehicle detection!
 
 **If you're feeling ambitious** (also totally optional though), don't stop there!  We encourage you to go out and take video of your own, and show us how you would implement this project on a new video!
+
+
+<table>
+  <tr>
+    <td align="center">HSV</td>
+    <td align="center">94.9%</td>
+  </tr>
+  <tr>
+    <td align="center">RGB</td>
+    <td align="center">90.5%</td>
+  </tr>
+  <tr>
+    <td align="center"><b>YCrCb</b></td>
+    <td align="center"><b>98.3%</b></td>
+  </tr>
+  <tr>
+    <td align="center">LUV</td>
+    <td align="center">97.0%</td>
+  </tr>
+  <tr>
+    <td align="center">HLS</td>
+    <td align="center">95.0%</td>
+  </tr>
+  <tr>
+    <td align="center">YUV</td>
+    <td align="center">97.6%</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <td align="center">HOG Channels</td>
+    <td align="center"><b>'ALL'</b></td>
+    <td align="center">0</td>
+    <td align="center">1</td>
+    <td align="center">2</td>
+  </tr>
+  <tr>
+    <td align="center">Validation accuracy</td>
+    <td align="center"><b>98.3%</b></td>
+    <td align="center">90.9%</td>
+    <td align="center">92.1%</td>
+    <td align="center">84.0%</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <td align="center">Feature</td>
+    <td align="center">Spatial</td>
+    <td align="center">Hist_Color</td>
+  </tr>
+  <tr>
+    <td align="center">True</td>
+    <td align="center">96.2%</td>
+    <td align="center"><b>98.3%</b></td>
+  </tr>
+  <tr>
+    <td align="center">False</td>
+    <td align="center"><b>98.3%</b></td>
+    <td align="center">97.7%</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <td align="center" colspan="5"><b>Using PCA for dimensionality reduction</b></th>
+  </tr>
+  <tr>
+    <td align="center"></td>
+    <td align="center">Number of features</td>
+    <td align="center">Explained variance</td>
+    <td align="center">Validation accuracy</td>
+    <td align="center">Testing accuracy</td>
+  </tr>
+  <tr>
+    <td align="center">Before PCA</td>
+    <td align="center">1020</td>
+    <td align="center">100.0%</td>
+    <td align="center">98.1%</td>
+    <td align="center">98.3%</td>
+  </tr>
+  <tr>
+    <td align="center">After PCA</td>
+    <td align="center">500</td>
+    <td align="center">98.6%</td>
+    <td align="center">97.6%</td>   
+    <td align="center">98.4%</td>
+  </tr>
+</table>
--- P5-Vehicle-Detection/utils/utils.py
+++ P5-Vehicle-Detection/utils/utils.py
@@ -241,3 +241,104 @@ def search_windows(img, windows, clf, scaler, pca, color_space=None,
             on_windows.append(window)
     #9) Return windows for positive detections
     return on_windows
+
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, pca, orient, pix_per_cell, cell_per_block, hist_bins, color_space, get_all = False):
+    img_tosearch = img[ystart:ystop,:,:]
+
+    ctrans_tosearch = convert_color(img_tosearch, conv='BGR2' + color_space)
+
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
+
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
+    nfeat_per_block = orient*cell_per_block**2
+
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step + 1
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step + 1
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    on_windows = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step
+            xpos = xb*cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
+
+            # Get color features
+            hist_features = color_hist(subimg, nbins=hist_bins)
+
+            # Scale features and make a prediction
+            test_features = X_scaler.transform(np.hstack((hist_features, hog_features)).reshape(1, -1))
+
+            reduced_features = pca.transform(test_features)
+
+            test_prediction = svc.predict(reduced_features)
+
+            if test_prediction == 1 or get_all == True:
+                xbox_left = np.int(xleft*scale)
+                ytop_draw = np.int(ytop*scale)
+                win_draw = np.int(window*scale)
+                on_windows.append(((xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart)))
+
+    return on_windows
+
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels, color = (0,0,255), thickness = 6):
+    rects = []
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        rects.append(bbox)
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], color, thickness)
+    # Return the image
+    return img, rects
